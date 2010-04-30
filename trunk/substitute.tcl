35da184c3dde760e145f3274f4c172a7ce5b17d6
rutgren <rutgren@7b1e8129-9946-0410-bb49-71228a181e99>
2010-04-30 09:42:25 +0000

    Initial commit of substitute.tcl

---
+++ trunk/substitute.tcl
@@ -0,0 +1,126 @@
+# substitute.tcl --
+#
+#     This script provides perl style text subsitution, with full regular
+#     expression support.
+#
+# Copyright (c) 2010, Rickard Utgren <rutgren@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# RCS: $Id: http-title.tcl 35 2010-04-29 20:25:59Z rutgren $
+#
+# Notes:
+#     Flags (i.e. global, case-insensitive) aren't supported directly in the
+#     syntax, however Tcl metasyntax can still be used. Expressions default to
+#     global/all, case-sensitive. Note that it's not possible to turn on or off
+#     global matching via metasyntax, this is because of a limitation in Tcl.
+#
+# To enable: .chanset #chan +substitute
+#
+# Usage examples:
+#     Basic usage
+#     <nick> Hello, World!
+#     <nick> s/World/Everyone
+#     <bot> Nick meant: "Hello, Everyone!"
+#
+#     With : instead of /
+#     <nick> Hello, World!
+#     <nick> s:World:Everyone
+#     <bot> Nick meant: "Hello, Everyone!"
+#
+#     Removing text
+#     <nick> Hello, World!
+#     <nick> s/,/
+#     <bot> Nick meant: "Hello World!"
+#
+#     Substitutions can be stacked indefinately
+#     <nick> Hello, World!
+#     <nick> s/l/w/s/o/u
+#     <bot> Nick meant: "Hewwu, Wurwd!"
+#
+#     You can keep stacking over multiple messages
+#     <nick> Hello, World!
+#     <nick> s/World/Everyone
+#     <bot> Nick meant: "Hello, Everyone!"
+#     <nick> s/Everyone/Friday
+#     <bot> Nick meant: "Hello, Friday!"
+#
+#     Regular expression
+#     <nick> Hello, World!
+#     <nick> s/[A-Z]/Y
+#     <bot> Nick meant: "Yello, Yorld!"
+#
+#     More regular expression
+#     <nick> Hello, World!
+#     <nick> s/\S/x
+#     <bot> Nick meant: "xxxxxx xxxxxx"
+#
+#     Metasyntax in action, turning on case insensitive matching:
+#     <nick> Aaa Bbb
+#     <nick> s/(?i)a/x/s/(?i)b/y
+#     <bot> Nick meant: "xxx yyy"
+#
+# v1.0 by Pixelz (rutgren@gmail.com), April 30, 2010
+
+package require eggdrop 1.6
+package require Tcl 8.5
+
+namespace eval ::substitute {
+	# minimum time in seconds before spoken lines are allowed to expire
+	variable dbExpire 600 ;# 10 minutes
+	setudef flag {substitute}
+	variable lastLine
+}
+
+proc ::substitute::pubm_substitute {nick uhost hand chan text} {
+	variable lastLine
+	if {![channel get $chan {substitute}]} {
+		return
+	} elseif {[string match {s[/:]*[/:]*} $text]} {
+		if {[info exists lastLine([set lnick [string tolower $nick]],$chan)]} {
+			set newLine [set last [lindex $lastLine($lnick,$chan) 1]]
+			foreach {- a b c d} [regexp -all -inline -- {s(?:/([^/]+)/([^/]*)|:([^:]+):([^:]*))} $text] {
+				if {$a ne {}} {
+					catch {set newLine [regsub -all -- $a $newLine $b]}
+				} else {
+					catch {set newLine [regsub -all -- $c $newLine $d]}
+				}
+			}
+			if {($newLine ne $last) && ([string length $newLine] <= 400)} {
+				putserv "PRIVMSG $chan :$nick meant: \"$newLine\""
+				set lastLine($lnick,$chan) [list [clock seconds] $newLine]
+				return
+			}
+		}
+	} else {
+		set lastLine([string tolower $nick],$chan) [list [clock seconds] $text]
+		return
+	}
+	return
+}
+
+proc ::substitute::dbCleanup {args} {
+	variable lastLine; variable dbExpire
+	foreach item [array names lastLine] {
+		if {([clock seconds] - [lindex $lastLine($item) 0]) > $dbExpire} {
+			unset lastLine($item)
+		}
+	}
+	return
+}
+
+namespace eval ::substitute {
+	bind pubm - "*" ::substitute::pubm_substitute
+	bind time - "?0 * * * *" ::substitute::dbCleanup
+	putlog "Loaded substitute.tcl v1.0 by Pixelz"
+}
